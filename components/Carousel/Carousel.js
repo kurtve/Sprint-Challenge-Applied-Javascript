f35a7557ba0bfabfc2574fd21f3974b66917ae46
Kurt Van Etten <kurt@vanetten.us>
2019-08-18 21:44:42 -0400

    Add carousel functionality (no animations yet)

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,81 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+
+// the image list
+const images = [
+    "./assets/carousel/mountains.jpeg",
+    "./assets/carousel/computer.jpeg",
+    "./assets/carousel/trees.jpeg",
+    "./assets/carousel/turntable.jpeg"
+];
+
+
+// generate a tab div element given a topic
+const makeImg = (image, idx) => {
+    const img = document.createElement('img');
+    img.src = image;
+    img.className = `carousel-img${idx}`;
+    return img;
+};
+
+
+// generate the carousel and attach to the DOM
+const attach = document.querySelector('.carousel-container');
+
+const carousel = document.createElement('div');
+carousel.className = 'carousel';
+
+const leftButton = document.createElement('div');
+leftButton.className = 'left-button';
+leftButton.textContent = '<';
+carousel.appendChild(leftButton);
+
+images.forEach((image, idx) => {
+    carousel.appendChild(makeImg(image, idx));
+});
+
+
+const rightButton = document.createElement('div');
+rightButton.className = 'right-button';
+rightButton.textContent = '>';
+carousel.appendChild(rightButton);
+
+attach.appendChild(carousel);
+
+
+// set up button functionality
+// Initialize number of images and start with image 0
+let carouselImage = 0;
+const numImages = images.length;
+
+let currentImage = document.querySelector(`.carousel-img${carouselImage}`);
+currentImage.classList.toggle('display-image');
+
+// left button click handler
+const shiftLeft = () => {
+    currentImage.classList.toggle('display-image');
+    carouselImage -= 1;
+    if (carouselImage < 0) {
+        carouselImage = numImages - 1;
+    };
+    currentImage = document.querySelector(`.carousel-img${carouselImage}`);
+    currentImage.classList.toggle('display-image');
+};
+
+// right button click handler
+const shiftRight = () => {
+    currentImage.classList.toggle('display-image');
+    carouselImage += 1;
+    if (carouselImage >= numImages) {
+        carouselImage = 0;
+    };
+    currentImage = document.querySelector(`.carousel-img${carouselImage}`);
+    currentImage.classList.toggle('display-image');
+};
+
+// add handlers to the buttons
+rightButton.addEventListener('click', shiftRight);
+leftButton.addEventListener('click', shiftLeft);
+
